9f5f0cacf5986decb4a1bbd977c2aad052ce37d1
Ismail Ahmed <130091934+iyahmed@users.noreply.github.com>
2024-04-07 14:15:00 -0700

    Second Commit: Part 7 Done

--- ch02/ColoredPoints.js
+++ ch02/ColoredPoints.js
@@ -69,7 +69,7 @@ function addActionsForHTMLUI() {
   // Button Events (Shape Type)
   document.getElementById('green').onclick = function() {g_selectedColor = [0.0, 1.0, 0.0, 1.0];};
   document.getElementById('red').onclick = function() {g_selectedColor = [1.0, 0.0, 0.0, 1.0];};
-  document.getElementById('clearButton').onclick = function() {g_shapesList = [];};
+  document.getElementById('clearButton').onclick = function() {g_shapesList = []; renderAllShapes();};
   
   // Color Slider Events
   document.getElementById('redSlide').addEventListener('mouseup', function() {g_selectedColor[0] = this.value / 100;});
